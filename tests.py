77a81ced581526d6b465a1dfd12f7ca1be9b9304
Jwink3101 <Jwink3101@users.noreply.github.com>
2018-11-01 10:16:38 -0600

    Uncommented some tests. Added threaded test

--- tests.py
+++ tests.py
@@ -46,89 +46,95 @@ def sleep(ii,t=0.05):
     return ii
 
 
-# print('--- Regular ---')
-# with Timer():
-#     list(map(sleep,range(10)))
-# with Timer():
-#     list(parmap.parmap(sleep,range(10),N=2))
-# with Timer():
-#     list(parmap.parmap(sleep,range(10),N=4))
-# 
-# print('--- Set KW fixed ---')
-# # Set the elapsed time. From now on, use parmap with N=1
-# for N in [1,2,4]:
-#     with Timer():
-#         list(parmap.parmap(sleep,range(10),N=N,kwargs={'t':0.01}))
-# 
-# print('--- Set KW as args fixed ---')
-# # Set the elapsed time. From now on, use parmap with N=1
-# for N in [1,2,4]:
-#     with Timer():
-#         list(parmap.parmap(sleep,range(10),N=N,args=(0.01,) ))
-# 
-# print('--- Set KW varied ---')
-# # Set the elapsed time. From now on, use parmap with N=1
-# for N in [1,2,4]:
-#     with Timer():
-#         list(parmap.parmap(sleep,((i,{'t':i/40.0}) for i in range(10)),N=N,
-#                            kwstar=True))
-# print('--- Set KW as arg varied ---')
-# # Set the elapsed time. From now on, use parmap with N=1
-# for N in [1,2,4]:
-#     with Timer():
-#         list(parmap.parmap(sleep,((i,i/40.0) for i in range(10)),N=N,
-#                            star=True))
-# 
-# 
-# def addsleep(ii,jj,t=0.05):
-#     time.sleep(t)
-#     with LOCK:
-#         print(ii,jj,t,end=' ')
-#     return ii+jj
-# rgold = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
-# 
-# print('--- star ---')
-# for N in [1,2,4]:
-#     with Timer():
-#         r = list(parmap.parmap(addsleep,zip(range(10),range(10)),
-#                                N=N,star=True))
-#         assert r == rgold
-# print('--- star with kw time ---')
-# for N in [1,2,4]:
-#     with Timer():
-#         r = list(parmap.parmap(addsleep,zip(range(10),range(10)),
-#                                N=N,star=True,kwargs={'t':0.01}))
-#         assert r == rgold
-# print('--- star with kw time as arg ---')
-# for N in [1,2,4]:
-#     with Timer():
-#         r = list(parmap.parmap(addsleep,zip(range(10),range(10)),
-#                                N=N,star=True,args=(0.01,)))
-#         assert r == rgold
-#         
-# print('--- star with kwstar time varied ---')
-# for N in [1,2,4]:
-#     with Timer():
-#         args = zip(range(10),range(10))
-#         kwargs = [{'t':i/40.0} for i in range(10)]
-#         r = list(parmap.parmap(addsleep,zip(args,kwargs),
-#                                N=N,star=True,kwstar=True))
-#         assert r == rgold
-# print('--- star with arg time varied via star ---')
-# for N in [1,2,4]:
-#     with Timer():
-#         args = zip(range(10),range(10),[i/40.0 for i in range(10)])
-#         r = list(parmap.parmap(addsleep,args,
-#                                N=N,star=True))
-#         assert r == rgold
-# print('--- star with kwstar time varied OVERRIDE kwarg ---')
-# for N in [1,2,4]:
-#     with Timer():
-#         args = zip(range(10),range(10))
-#         kwargs = [{'t':i/40.0} for i in range(10)]
-#         r = list(parmap.parmap(addsleep,zip(args,kwargs),
-#                                N=N,star=True,kwstar=True,
-#                                kwargs={'t':1}))
+print('--- Regular ---')
+with Timer():
+    list(map(sleep,range(10)))
+with Timer():
+    list(parmap.parmap(sleep,range(10),N=2))
+with Timer():
+    list(parmap.parmap(sleep,range(10),N=4))
+
+print('--- Set KW fixed ---')
+# Set the elapsed time. From now on, use parmap with N=1
+for N in [1,2,4]:
+    with Timer():
+        list(parmap.parmap(sleep,range(10),N=N,kwargs={'t':0.01}))
+
+print('--- Set KW as args fixed ---')
+# Set the elapsed time. From now on, use parmap with N=1
+for N in [1,2,4]:
+    with Timer():
+        list(parmap.parmap(sleep,range(10),N=N,args=(0.01,) ))
+
+print('--- Set KW varied ---')
+# Set the elapsed time. From now on, use parmap with N=1
+for N in [1,2,4]:
+    with Timer():
+        list(parmap.parmap(sleep,((i,{'t':i/40.0}) for i in range(10)),N=N,
+                           kwstar=True))
+print('--- Set KW as arg varied ---')
+# Set the elapsed time. From now on, use parmap with N=1
+for N in [1,2,4]:
+    with Timer():
+        list(parmap.parmap(sleep,((i,i/40.0) for i in range(10)),N=N,
+                           star=True))
+
+
+def addsleep(ii,jj,t=0.05):
+    time.sleep(t)
+    with LOCK:
+        print(ii,jj,t,end=' ')
+    return ii+jj
+rgold = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+
+print('--- star ---')
+for N in [1,2,4]:
+    with Timer():
+        r = list(parmap.parmap(addsleep,zip(range(10),range(10)),
+                               N=N,star=True))
+        assert r == rgold
+print('--- star with kw time ---')
+for N in [1,2,4]:
+    with Timer():
+        r = list(parmap.parmap(addsleep,zip(range(10),range(10)),
+                               N=N,star=True,kwargs={'t':0.01}))
+        assert r == rgold
+print('--- star with kw time as arg ---')
+for N in [1,2,4]:
+    with Timer():
+        r = list(parmap.parmap(addsleep,zip(range(10),range(10)),
+                               N=N,star=True,args=(0.01,)))
+        assert r == rgold
+        
+print('--- star with kwstar time varied ---')
+for N in [1,2,4]:
+    with Timer():
+        args = zip(range(10),range(10))
+        kwargs = [{'t':i/40.0} for i in range(10)]
+        r = list(parmap.parmap(addsleep,zip(args,kwargs),
+                               N=N,star=True,kwstar=True))
+        assert r == rgold
+print('--- star with arg time varied via star ---')
+for N in [1,2,4]:
+    with Timer():
+        args = zip(range(10),range(10),[i/40.0 for i in range(10)])
+        r = list(parmap.parmap(addsleep,args,
+                               N=N,star=True))
+        assert r == rgold
+print('--- star with kwstar time varied OVERRIDE kwarg ---')
+for N in [1,2,4]:
+    with Timer():
+        args = zip(range(10),range(10))
+        kwargs = [{'t':i/40.0} for i in range(10)]
+        r = list(parmap.parmap(addsleep,zip(args,kwargs),
+                               N=N,star=True,kwstar=True,
+                               kwargs={'t':1}))
+
+print('--- Threads ---')
+# time.sleep releases the GIL so this should be about the same.
+for N in [1,2,4]:
+    with Timer():
+        list(parmap.parmap(sleep,range(10),N=1,Nt=N,kwargs={'t':0.01}))
 
 print('--- Lambdas ---')
 # Make sure this (a) runs and (b) returns properly
